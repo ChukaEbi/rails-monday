932c4161995c15b995e91b71ffb180ece9ac8f92
Chuka Ebi <chuka.ebi@outlook.com>
2015-11-24 12:49:44 +0000

    Prevention of orphaned reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,7 +1,12 @@
 describe Review, type: :model do
   it { is_expected.to  belong_to :restaurant }
 
-  let!(:abokado) {Restaurant.create(name: 'Abokado')
+  let!(:abokado) {Restaurant.create(name: 'Abokado')}
+
   it 'should remove the review wheen the restaurant is deleted' do
+    r = Review.new(thoughts: "meh meh")
+    r.save!
+    abokado.reviews << r
+    expect{abokado.destroy}.to change {Review.count}.by(-1)
   end
 end
