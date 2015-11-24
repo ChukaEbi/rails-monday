5392668f3de537ad1c461002d05700cf9a6ccdb7
NULL-OPERATOR <x@cryptolab.net>
2015-11-24 14:57:33 +0000

    added tests, and validations of reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,9 +4,14 @@ describe Review, type: :model do
   let!(:abokado) {Restaurant.create(name: 'Abokado')}
 
   it 'should remove the review wheen the restaurant is deleted' do
-    r = Review.new(thoughts: "meh meh")
+    r = Review.new(thoughts: "meh meh", rating: 3)
     r.save!
     abokado.reviews << r
     expect{abokado.destroy}.to change {Review.count}.by(-1)
+  end
+
+  it "is invalid if the rating is more than 5" do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
   end
 end
